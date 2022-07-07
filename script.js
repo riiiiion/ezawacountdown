51a208d659aa6380e1d3ef471eacdb19ef1130a7
riiiiion <105769366+riiiiion@users.noreply.github.com>
2022-07-07 17:43:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
 
 function CountdownTimer(elm, tl, mes) {
-this.initialize.apply(this, arguments);
+    this.initialize.apply(this, arguments);
 }
 CountdownTimer.prototype = {
-initialize: function (elm, tl, mes) {
-this.elem = document.getElementById(elm);
-this.tl = tl;
-this.mes = mes;
-}, countDown: function () {
-var timer = '';
-var today = new Date();
-var day = Math.floor((this.tl - today) / (24 * 60 * 60 * 1000));
-var hour = Math.floor(((this.tl - today) % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
-var min = Math.floor(((this.tl - today) % (24 * 60 * 60 * 1000)) / (60 * 1000)) % 60;
-var sec = Math.floor(((this.tl - today) % (24 * 60 * 60 * 1000)) / 1000) % 60 % 60;
-var milli = Math.floor(((this.tl - today) % (24 * 60 * 60 * 1000)) / 10) % 100;
-var me = this;
+    initialize: function (elm, tl, mes) {
+        this.elem = document.getElementById(elm);
+        this.tl = tl;
+        this.mes = mes;
+    }, countDown: function () {
+        var timer = '';
+        var today = new Date();
+        var day = Math.floor((this.tl - today) / (24 * 60 * 60 * 1000));
+        var hour = Math.floor(((this.tl - today) % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+        var min = Math.floor(((this.tl - today) % (24 * 60 * 60 * 1000)) / (60 * 1000)) % 60;
+        var sec = Math.floor(((this.tl - today) % (24 * 60 * 60 * 1000)) / 1000) % 60 % 60;
+        var milli = Math.floor(((this.tl - today) % (24 * 60 * 60 * 1000)) / 10) % 100;
+        var me = this;
 
-if ((this.tl - today) > 0) {
-if (day)
-timer += '' + day + '日';
-if (hour)
-timer += '' + hour + '時間';
-timer += '' + this.addZero(min) + '分' +
-    this.addZero(sec) + '秒';
-this.elem.innerHTML = timer;
-tid = setTimeout(function () {
-me.countDown();
-}, 10);
-} else {
-this.elem.innerHTML = this.mes;
-return;
-}
-}, addZero: function (num) {
-return ('0' + num).slice(-2);
-}
+        if ((this.tl - today) > 0) {
+            if (day)
+                timer += '' + day + '日';
+            if (hour)
+                timer += '' + hour + '時間';
+            timer += '' + this.addZero(min) + '分' +
+                this.addZero(sec) + '秒';
+            this.elem.innerHTML = timer;
+            tid = setTimeout(function () {
+                me.countDown();
+            }, 10);
+        } else {
+            this.elem.innerHTML = this.mes;
+            return;
+        }
+    }, addZero: function (num) {
+        return ('0' + num).slice(-2);
+    }
 }
 function CDT() {
-var tl = new Date('2021/5/29 10:00:00');// ここで日付を指定
-var timer = new CountdownTimer('CDT', tl, '終了しました');
-timer.countDown();
+    var tl = new Date('2022/8/27 10:00:00');// ここで日付を指定
+    var timer = new CountdownTimer('CDT', tl, '終了しました');
+    timer.countDown();
 }
 
 
 
 window.onload = function () {
-CDT();
-CDT01();
+    CDT();
+    CDT01();
 }
